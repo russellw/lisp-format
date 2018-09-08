7d95dfa9d67562e922923b0a2ebb9bdc28506efe
Russell Wallace <russell.wallace@gmail.com>
2018-09-08 03:17:04 +0100

    read

--- read.lisp
+++ read.lisp
@@ -1,14 +1,9 @@
 (defun line-comment-reader (*standard-input* c)
-  (list +line-comment+
-    (concatenate 'string
-                     (string c)
-                     (read-line *standard-input* nil #\Newline t))))
+  (list +line-comment+ (concatenate 'string (string c) (read-line *standard-input* nil #\Newline t))))
 
 (defun feature-test-reader (*standard-input* c x)
-   (declare (ignore c x))
-   (list +feature-test+
-    (read *standard-input* t nil t)
-    (read *standard-input* t nil t)))
+  (declare (ignore c x))
+  (list +feature-test+ (read *standard-input* t nil t) (read *standard-input* t nil t)))
 
 (defun read-file (file)
   (with-open-file (*standard-input* file)
