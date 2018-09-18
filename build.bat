f8cd7b4956f7c9ce8516aba28c99dc1480f21c42
Russell Wallace <russell.wallace@gmail.com>
2018-09-18 07:37:45 +0100

    clean

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-sbcl --no-userinit --load main.lisp --eval "(sb-ext:save-lisp-and-die \"lisp-format.exe\" :toplevel 'main :executable t)"
+sbcl --load main.lisp --eval "(sb-ext:save-lisp-and-die \"lisp-format.exe\" :toplevel 'main :executable t)"
