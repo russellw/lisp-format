d77d8343f3e5479869245a5739517e254a244fe7
Russell Wallace <russell.wallace@gmail.com>
2018-09-23 07:36:25 +0100

    split-string

--- etc.lisp
+++ etc.lisp
@@ -1,6 +1,41 @@
-(defun substringp(needle haystack i)
+(defun subseqp(needle haystack i)
   (when(<=(+ i (length needle))(length haystack))
     (dotimes (j (length needle) t)
       (unless(eql(elt needle j)(elt haystack(+ i j)))
         (return nil)))))
 
+
+(defun split-string(delimiter s)
+  (let((i 0))
+    (labels
+      (
+        (skip-delimiters ()
+          (loop
+            while(subseqp delimiter s i)
+            do
+            (incf i(length delimiter))
+          )
+        )
+        (next-delimiter ()
+          (let((i
+              (search delimiter s :start2 i)))
+             (if i
+              i
+              (length s)))
+        )
+        (next-word ()
+          (skip-delimiters)
+          (let*((j(next-delimiter))
+                (word(subseq s i  j)))
+             (setf i j)
+             (if(equal word "")nil word))
+        )
+      )
+      (loop
+        while(< i(length s))
+        if(next-word)
+        collect it
+      )
+    )
+  )
+)
--- test.lisp
+++ test.lisp
@@ -1,4 +1,12 @@
 (load "etc")
-;substringp
-(assert(substringp "a" "abc" 0))
-(assert(not(substringp "a" "abc" 1)))
+;subseqp
+(assert(subseqp "a" "abc" 0))
+(assert(not(subseqp "a" "abc" 1)))
+(assert(subseqp "quick" "the quick brown fox jumped over the lazy dog" 4))
+(assert(not(subseqp "quick" "the quick brown fox jumped over the lazy dog" 5)))
+(assert(not(subseqp "quickened" "the quick brown fox jumped over the lazy dog" 4)))
+(assert(not(subseqp "quick" "the quick brown fox jumped over the lazy dog" 400)))
+;split-string
+(assert(equal(split-string " " "one two three")'("one" "two" "three")))
+(assert(equal(split-string " " " one two three")'("one" "two" "three")))
+(assert(equal(split-string " " "one two three ")'("one" "two" "three")))
