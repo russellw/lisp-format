aca6f8219eadcde78d5a8b6508d9ecb60adccaae
Russell Wallace <russell.wallace@gmail.com>
2018-10-04 16:43:09 +0100

    bats

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-sbcl --non-interactive --load main.lisp --eval "(sb-ext:save-lisp-and-die \"lisp-format.exe\" :toplevel 'main :executable t)"
+sbcl --noinform --disable-ldb --lose-on-corruption --non-interactive --load main.lisp --eval "(sb-ext:save-lisp-and-die \"lisp-format.exe\" :toplevel 'main :executable t)"
