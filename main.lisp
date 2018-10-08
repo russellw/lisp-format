a51c87b75bdebee9c4b714e915056596d9663207
Russell Wallace <russell.wallace@gmail.com>
2018-10-08 08:59:34 +0100

    fmt

--- main.lisp
+++ main.lisp
@@ -1,132 +1,112 @@
-;boilerplate
+; Boilerplate
 #+sbcl (declaim (muffle-conditions style-warning))
 
-;globals
+; Globals
 (defconstant +special+ (gensym))
 (defvar *right-margin* 80)
 
-;macros
+; Macros
 (load "macros")
 
-;modules
-(load "add-blanks")
+; Modules
 (load "etc")
-(load "read")
 (load "fmt")
+(load "read")
 
-;Code transformations
+; Optional transformations
 (load "comment-case")
 (load "comment-space")
 
-;options
+; Necessary transformations
+(load "add-blanks")
+
+; Options
 (defvar *comment-case* nil)
 (defvar *comment-space* nil)
 (defvar *all* nil)
 
-(defun args()
-      #+ccl *unprocessed-command-line-arguments*
-      #+sbcl (cdr *posix-argv*)
-)
+(defun args ()
+  #+ccl *unprocessed-command-line-arguments*
+  #+sbcl (cdr *posix-argv*))
 
-(defun canonical-option(s)
-  (when(and(search"Win"(software-type))
-           (subseqp"/"s ))
-    (when(equal s"/?")
-      (setf s "-h")
-    )
-    (setf(elt s 0 )#\-)
-  )
-  (loop while(and   (>(length s)2)
-                    (subseqp "--"s )
-             )
-    do
-    (setf s(subseq s 1))
-  )
-  s
-)
+(defun canonical-option (s)
+  (when (and (search "Win" (software-type)) (subseqp "/" s))
+    (when (equal s "/?")
+      (setf s "-h"))
+    (setf (elt s 0) #\-))
+  (loop
+   while (and (> (length s) 2) (subseqp "--" s))
+   do
+   (setf s (subseq s 1)))
+  s)
 
 (defun help ()
-(format t"General options:~%")
-(format t"-help           Show help~%")
-(format t"-version        Show version~%")
-(format t"~%")
-(format t"Code transformations:~%")
-(format t"-comment-case   ;; foo -> ;; Foo~%")
-(format t"-comment-space  ;;foo -> ;; foo~%")
-(format t"-all            All the above~%")
-)
+  (format t "General options:~%")
+  (format t "-help           Show help~%")
+  (format t "-version        Show version~%")
+  (format t "~%")
+  (format t "Code transformations:~%")
+  (format t "-comment-case   ;; foo -> ;; Foo~%")
+  (format t "-comment-space  ;;foo -> ;; foo~%")
+  (format t "-all            All the above~%"))
 
 (defun version ()
-(format t"lisp-format version 0~%")
-)
+  (format t "lisp-format version 0~%"))
 
 (defun parse-args (args)
-  (let (
-        (options t)
-       )
-      ;options
-      (loop while args
-        for s =(canonical-option (pop args))
-        if (and options (subseqp"-"s ))
-          do
-          (cond
-          ;General options
-            ((member s '("-h" "-help" ) :test #'string=)
-              (help))
-            ((member s '("-V" "-v" "-version") :test #'string=)
-              (version))
+  (let ((options t))
+    (loop
+     while args
+     for s
+     = (canonical-option (pop args))
+     if (and options (subseqp "-" s))
+     do
+     (cond
 
-              ;Code transformations
-              ((equal s"-comment-case")
-                (setf *comment-case* t))
-              ((equal s"-comment-space")
-                (setf *comment-space* t))
-              ((equal s"-all")
-                (setf *all* t))
+       ; General options
+       ((member s '("-h" "-help") :test #'string=)
+        (help))
+       ((member s '("-V" "-v" "-version") :test #'string=)
+        (version))
 
-              ;error
-            (t (format t "~a: unknown option~%"s))
-          )
-        else
-           collect s
-      )
-)
-)
+       ; Code transformations
+       ((equal s "-comment-case")
+        (setf *comment-case* t))
+       ((equal s "-comment-space")
+        (setf *comment-space* t))
+       ((equal s "-all")
+        (setf *all* t))
 
-(defun transform(s)
-              ;optional transformations
-              (when (or *comment-case* *all*)
-                (setf s(comment-case s)))
-              (when (or *comment-space* *all*)
-                (setf s(comment-space s)))
+       ; Error
+       (t
+        (format t "~a: unknown option~%"
+          s)))
+     else collect
+     s)))
 
+(defun transform (s)
 
+  ; Optional transformations
+  (when (or *comment-case* *all*)
+    (setf s (comment-case s)))
+  (when (or *comment-space* *all*)
+    (setf s (comment-space s)))
 
-        ;necessary transformations
-        (add-blanks s)
-)
+  ; Necessary transformations
+  (add-blanks s))
+
 (defun do-file (file)
-      (let ((new-text(fmt-all(transform (read-file file))))
-            (old-text(read-text-file file))
-            (backup (make-pathname
-                      :defaults file
-                      :directory "/tmp/")))
-        (unless(string= old-text new-text)
-      (format t "~a~%" file)
-        (ignore-errors
-          (delete-file backup))
-        (ignore-errors
-          (rename-file file backup))
-        (write-text-file file new-text)))
-)
+  (let ((new-text (fmt-all (transform (read-file file))))
+        (old-text (read-text-file file))
+        (backup (make-pathname :defaults file :directory "/tmp/")))
+    (unless (string= old-text new-text)
+      (format t "~a~%"
+        file)
+      (ignore-errors (delete-file backup))
+      (ignore-errors (rename-file file backup))
+      (write-text-file file new-text))))
 
 (defun main ()
-  (let (
-        (files(parse-args(args)))
-       )
-
+  (let ((files (parse-args (args))))
     (dolist (file files)
-      (do-file file)
-    )
-  )
-)
+      (do-file file))))
