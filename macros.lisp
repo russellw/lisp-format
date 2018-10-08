504d18619186218cea6503124176ee31a18742db
Russell Wallace <russell.wallace@gmail.com>
2018-10-08 02:35:20 +0100

    program, format thyself

--- macros.lisp
+++ macros.lisp
@@ -1,14 +1,17 @@
 (defmacro acond (&rest clauses)
-    (when clauses
-        (let ((clause (car clauses))
-              (name (gensym)))
-          `(let ((,name ,(car clause)))
-             (if ,name
-                 (let ((it ,name)) ,@(cdr clause))
-                 (acond ,@(cdr clauses)))))))
+  (when clauses
+    (let ((clause (car clauses))
+          (name (gensym)))
+      `(let ((,name ,(car clause)))
+         (if ,name
+           (let ((it ,name))
+             ,@(cdr clause))
+           (acond ,@(cdr clauses)))))))
 
-(defmacro pr(a)
-  (let((name(gensym)))
-    `(let((,name ,a))
-      (format t "~s = ~s~%" ',a ,name)
-      ,name)))
+(defmacro pr (a)
+  (let ((name (gensym)))
+    `(let ((,name ,a))
+       (format t "~s = ~s~%"
+         ',a
+         ,name)
+       ,name)))
