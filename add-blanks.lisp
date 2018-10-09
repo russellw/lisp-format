b29afdca3df22585afde19c8cfe375a197819cbf
Russell Wallace <russell.wallace@gmail.com>
2018-10-09 17:17:51 +0100

    blanks

--- add-blanks.lisp
+++ add-blanks.lisp
@@ -1,46 +1,42 @@
-
 (defun remove-extra-blanks (s)
-  ;remove leading blanks
+  ; Remove leading blanks
   (loop
    while (blankp (car s))
    do
    (pop s))
-   ;remove consecutive blanks
-  (setf s
+  ; Remove consecutive blanks
+  (setf
+    s
+    (loop
+     for (a . more) on s
+     when (not (blankp a))
+     collect a
+     when (and (blankp a) (not (blankp (car more))))
+     collect a))
+  ; Remove trailing blanks, except after line comments
+  (setf s (reverse s))
   (loop
-   for (a . more) on s
-   when (not (blankp a))
-   collect a
-   when (and (blankp a) (not (blankp (car more))))
-   collect a)
-   )
-  ;remove trailing blanks, except after line comments
-  (setf  s(reverse s))
-  (loop
-   while(and (blankp (car s))(not(line-comment-p(cadr s))))
+   while (and (blankp (car s)) (not (line-comment-p (cadr s))))
    do
    (pop s))
-  )
-  (reverse s)
-   )
+  (reverse s))
 
 (defun add-blanks (a)
   (if (atom a)
     a
-    (let(( s (mapcar #'add-blanks a)))
+    (let ((s (mapcar #'add-blanks a)))
       (remove-extra-blanks
         (loop
          for (a . more) on s
-         with  b = (car more)
-
-         if (and(consp a)(member (car a) ' (defmacro defun)))
-          collect (list +special+ "")
-
+         with b = (car more)
+         ; Blank around big top-level definition
+         if (and (consp a) (member (car a) ' (defmacro defun)))
+         collect (list +special+ "")
+         ; Current form
          collect a
-
-         if(and(not(line-comment-p a))(line-comment-p b))
-          collect (list +special+ "")
-         if (and(consp a)(member (car a) ' (defmacro defun)))
-          collect (list +special+ "")
-
-         )))))
+         ; Blank before line comments
+         if (and (not (line-comment-p a)) (line-comment-p b))
+         collect (list +special+ "")
+         ; Blank around big top-level definition
+         if (and (consp a) (member (car a) ' (defmacro defun)))
+         collect (list +special+ ""))))))
